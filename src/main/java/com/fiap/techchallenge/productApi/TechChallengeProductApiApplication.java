af1cb3b7cb3002117343e5ee8ffa01ef166f640a
Guilherme Camargo <guilherme.camargo@amedigital.com>
2024-02-26 20:23:56 -0300

    criando projeto

---
+++ src/main/java/com/fiap/techchallenge/productApi/TechChallengeProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.productApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/techchallenge/productApi/TechChallengeProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.productApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
